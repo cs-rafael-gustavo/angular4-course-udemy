cd42c034a961d9a64db5511271912d72b91671ef
Rafael Matoso <rafaelmatosog@gmail.com>
2020-05-21 17:50:33 -0300

    Lesson 24, 25 Done

--- 02_typescript_oo/dist/app.js
+++ 02_typescript_oo/dist/app.js
@@ -1,4 +1,5 @@
 "use strict";
+var _a;
 var mensagem = "Seja bem vindo";
 var temporadasFriends = 10;
 var estudandoAngular = true;
@@ -6,3 +7,9 @@ var listaDeFrutas = ["Uva", "Banana", "Abacaxi"];
 var listaDeFrutas2 = ["Uva", "Banana", "Abacaxi"];
 var notaDasProvas = [7.5, 8, 9];
 var notaDasProvas2 = [7.5, 8, 9];
+var a = 1;
+var b = 2;
+var c = [];
+_a = [b, a], a = _a[0], b = _a[1];
+console.log("a: ", a);
+console.log("b: ", b);
