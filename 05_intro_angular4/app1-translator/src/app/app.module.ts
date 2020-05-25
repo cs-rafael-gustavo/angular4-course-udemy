cbd60248ec98e4a2f3c6ef88a7786970af7f7b0a
Rafael Matoso <rafaelmatosog@gmail.com>
2020-05-25 14:52:43 -0300

    Lesson 44 Done

--- 05_intro_angular4/app1-translator/src/app/app.module.ts
+++ 05_intro_angular4/app1-translator/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopoComponent } from './components/topo/topo.component';
 import { PainelComponent } from './components/painel/painel.component';
+import { TentativasComponent } from './components/tentativas/tentativas.component';
+import { ProgressoComponent } from './components/progresso/progresso.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TopoComponent,
-    PainelComponent
+    PainelComponent,
+    TentativasComponent,
+    ProgressoComponent
   ],
   imports: [
     BrowserModule,
