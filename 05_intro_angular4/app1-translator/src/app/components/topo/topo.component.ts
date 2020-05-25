3f8bd79d85b7572e9585fda9a0ee638510db8f98
Rafael Matoso <rafaelmatosog@gmail.com>
2020-05-25 14:22:53 -0300

    Lesson 39, 40 Done

---
+++ 05_intro_angular4/app1-translator/src/app/components/topo/topo.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-topo",
+  templateUrl: "./topo.component.html",
+  styles: [],
+})
+export class TopoComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
