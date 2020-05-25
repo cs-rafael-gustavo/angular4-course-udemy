1cd7ef73bd4043ea2cc62d4b129e86d895282ebc
Rafael Matoso <rafaelmatosog@gmail.com>
2020-05-25 14:48:23 -0300

    Lesson 43 Done

--- 05_intro_angular4/app1-translator/src/app/app.module.ts
+++ 05_intro_angular4/app1-translator/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopoComponent } from './components/topo/topo.component';
+import { PainelComponent } from './components/painel/painel.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopoComponent
+    TopoComponent,
+    PainelComponent
   ],
   imports: [
     BrowserModule,
