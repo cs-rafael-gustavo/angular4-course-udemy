04258639ef4acb7ac69cb3606080181f1c43157c
Rafael Matoso <rafaelmatosog@gmail.com>
2020-05-25 21:56:09 -0300

    Lesson 56 Done: import phrases to Painel component

--- 05_intro_angular4/app1-translator/src/app/components/painel/painel.component.ts
+++ 05_intro_angular4/app1-translator/src/app/components/painel/painel.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+
+import { Frase } from "../../shared/frase.model";
+import { FRASES } from "./frases-mock";
 
 @Component({
-  selector: 'app-painel',
-  templateUrl: './painel.component.html',
-  styleUrls: ['./painel.component.css']
+  selector: "app-painel",
+  templateUrl: "./painel.component.html",
+  styleUrls: ["./painel.component.css"],
 })
 export class PainelComponent implements OnInit {
+  public frases: Frase[] = FRASES;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    console.log(this.frases);
   }
 
+  ngOnInit(): void {}
 }
