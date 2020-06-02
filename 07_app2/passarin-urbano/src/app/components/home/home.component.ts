f19f882c452448373c2f267fa71b64938fc84a22
Rafael Matoso <rafaelmatosog@gmail.com>
2020-06-02 12:12:05 -0300

    Lesson 96 Done

--- 07_app2/passarin-urbano/src/app/components/home/home.component.ts
+++ 07_app2/passarin-urbano/src/app/components/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { OfertasService } from "../../ofertas.service";
+import { Oferta } from "../shared/oferta.model";
 @Component({
   selector: "app-home",
   templateUrl: "./home.component.html",
@@ -7,9 +8,12 @@ import { OfertasService } from "../../ofertas.service";
   providers: [OfertasService],
 })
 export class HomeComponent implements OnInit {
+  public ofertas: Array<Oferta>;
+
   constructor(private ofertasServices: OfertasService) {}
 
   ngOnInit(): void {
-    console.log(this.ofertasServices.getOfertas());
+    this.ofertas = this.ofertasServices.getOfertas();
+    console.log(this.ofertas);
   }
 }
