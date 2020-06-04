ad18efee95bdaf47a0adbbe8f091ca6b8dc8759a
Rafael Matoso <rafaelmatosog@gmail.com>
2020-06-04 17:46:04 -0300

    Lesson 121 Done

--- 07_app2/passarin-urbano/src/app/app-routing.module.ts
+++ 07_app2/passarin-urbano/src/app/app-routing.module.ts
@@ -10,6 +10,7 @@ const routes: Routes = [
   { path: "restaurantes", component: RestaurantesComponent },
   { path: "diversao", component: DiversaoComponent },
   { path: "oferta", component: OfertaComponent },
+  { path: "oferta/:id", component: OfertaComponent },
 ];
 
 @NgModule({
