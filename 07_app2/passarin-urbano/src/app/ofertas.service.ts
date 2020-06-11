4d848091e057e5da6a8ac0f6a5a214290c65d49f
Rafael Matoso <rafaelmatosog@gmail.com>
2020-06-11 11:57:41 -0300

    Lesson 126 to 128 update Done

--- 07_app2/passarin-urbano/src/app/ofertas.service.ts
+++ 07_app2/passarin-urbano/src/app/ofertas.service.ts
@@ -1,29 +1,31 @@
 import { HttpClient } from "@angular/common/http";
 import { Oferta } from "./components/shared/oferta.model";
 import { Injectable } from "@angular/core";
-import { environment } from "../environments/environment";
+import { URL_API } from "./app.api";
 
 @Injectable()
 export class OfertasService {
+  private serviceRoute: string = "ofertas";
+
   constructor(private http: HttpClient) {}
 
   public getOfertas(): Promise<Array<Oferta>> {
     return this.http
-      .get(`${environment.API_URL}ofertas?destaque=true`)
+      .get(`${URL_API}${this.serviceRoute}?destaque=true`)
       .toPromise()
       .then((res: any) => res);
   }
 
   public getOfertasPorCategoria(categoria: string): Promise<Array<Oferta>> {
     return this.http
-      .get(`${environment.API_URL}ofertas?categoria=${categoria}`)
+      .get(`${URL_API}${this.serviceRoute}?categoria=${categoria}`)
       .toPromise()
       .then((res: any) => res);
   }
 
   public getOfertaPorId(id: number): Promise<Oferta> {
     return this.http
-      .get(`${environment.API_URL}ofertas?id=${id}`)
+      .get(`${URL_API}${this.serviceRoute}?id=${id}`)
       .toPromise()
       .then((res: any) => res.shift());
   }
