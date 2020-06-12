48015f28d6b923fd943ffd75173090b0d456ec35
Rafael Matoso <rafaelmatosog@gmail.com>
2020-06-12 10:07:05 -0300

    Lesson 129 Done

--- 07_app2/passarin-urbano/src/app/ofertas.service.ts
+++ 07_app2/passarin-urbano/src/app/ofertas.service.ts
@@ -5,27 +5,25 @@ import { URL_API } from "./app.api";
 
 @Injectable()
 export class OfertasService {
-  private serviceRoute: string = "ofertas";
-
   constructor(private http: HttpClient) {}
 
   public getOfertas(): Promise<Array<Oferta>> {
     return this.http
-      .get(`${URL_API}${this.serviceRoute}?destaque=true`)
+      .get(`${URL_API}?destaque=true`)
       .toPromise()
       .then((res: any) => res);
   }
 
   public getOfertasPorCategoria(categoria: string): Promise<Array<Oferta>> {
     return this.http
-      .get(`${URL_API}${this.serviceRoute}?categoria=${categoria}`)
+      .get(`${URL_API}?categoria=${categoria}`)
       .toPromise()
       .then((res: any) => res);
   }
 
   public getOfertaPorId(id: number): Promise<Oferta> {
     return this.http
-      .get(`${URL_API}${this.serviceRoute}?id=${id}`)
+      .get(`${URL_API}?id=${id}`)
       .toPromise()
       .then((res: any) => res.shift());
   }
