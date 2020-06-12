cf939f1c5def6d67891df516b72260babb754c40
Rafael Matoso <rafaelmatosog@gmail.com>
2020-06-12 10:17:05 -0300

    Lesson 131 Done

--- 07_app2/passarin-urbano/src/app/app-routing.module.ts
+++ 07_app2/passarin-urbano/src/app/app-routing.module.ts
@@ -4,13 +4,23 @@ import { HomeComponent } from "./pages/home/home.component";
 import { RestaurantesComponent } from "./pages/restaurantes/restaurantes.component";
 import { DiversaoComponent } from "./pages/diversao/diversao.component";
 import { OfertaComponent } from "./pages/oferta/oferta.component";
+import { OndeFicaComponent } from "./pages/oferta/onde-fica/onde-fica.component";
+import { ComoUsarComponent } from "./pages/oferta/como-usar/como-usar.component";
 
 const routes: Routes = [
   { path: "", component: HomeComponent },
   { path: "restaurantes", component: RestaurantesComponent },
   { path: "diversao", component: DiversaoComponent },
   { path: "oferta", component: OfertaComponent },
-  { path: "oferta/:id", component: OfertaComponent },
+  {
+    path: "oferta/:id",
+    component: OfertaComponent,
+    children: [
+      { path: "", component: ComoUsarComponent },
+      { path: "como-usar", component: ComoUsarComponent },
+      { path: "onde-fica", component: OndeFicaComponent },
+    ],
+  },
 ];
 
 @NgModule({
