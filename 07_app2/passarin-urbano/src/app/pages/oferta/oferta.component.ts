e466eecd5e8697784323cc242e69e5c23b2b0e19
Rafael Matoso <rafaelmatosog@gmail.com>
2020-06-12 16:18:47 -0300

    Lesson 141 Done

--- 07_app2/passarin-urbano/src/app/pages/oferta/oferta.component.ts
+++ 07_app2/passarin-urbano/src/app/pages/oferta/oferta.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
 import { OfertasService } from "../../ofertas.service";
 import { Oferta } from "./../../components/shared/oferta.model";
+import { Observable, Observer } from "rxjs";
+import { interval } from "rxjs";
 
 @Component({
   selector: "app-oferta",
@@ -24,14 +26,28 @@ export class OfertaComponent implements OnInit {
         this.oferta = oferta;
       });
 
-    this.route.params.subscribe(
-      (params: any) => {
-        console.log(params);
-      },
-      (err: any) => {
-        console.log(err);
-      },
-      () => console.log("Processamento classificado como concluído")
-    );
+    // this.route.params.subscribe(
+    //   (params: any) => {
+    //     console.log(params);
+    //   },
+    //   (err: any) => {
+    //     console.log(err);
+    //   },
+    //   () => console.log("Processamento classificado como concluído")
+    // );
+
+    // let tempo = interval(1000);
+    // tempo.subscribe((interval: number) => {
+    //   console.log(interval);
+    // });
+
+    // observable (observável)
+    let meuObservableTeste = Observable.create((observer: Observer<number>) => {
+      observer.next(1);
+      observer.next(2);
+    });
+
+    // observer (observador)
+    meuObservableTeste.subscribe((res: number) => console.log(res + 10));
   }
 }
