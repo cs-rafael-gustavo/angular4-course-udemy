1cf9af30a5257cc684c01b8824d6f5f48f2422e1
Rafael Matoso <rafaelmatosog@gmail.com>
2020-06-15 16:53:05 -0300

    Lesson 151 Done: Learn about HttpResponse

--- 07_app2/passarin-urbano/src/app/ofertas.service.ts
+++ 07_app2/passarin-urbano/src/app/ofertas.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpResponse } from "@angular/common/http";
 import { Oferta } from "./components/shared/oferta.model";
 import { Injectable } from "@angular/core";
 import { URL_API } from "./app.api";
@@ -9,32 +9,34 @@ import { map, retry } from "rxjs/operators";
 export class OfertasService {
   constructor(private http: HttpClient) {}
 
-  public getOfertas(): Promise<Array<Oferta>> {
+  public getOfertas(): Promise<HttpResponse<Oferta[]>> {
     return this.http
       .get(`${URL_API}/ofertas?destaque=true`)
       .toPromise()
-      .then((res: any) => res);
+      .then((res: HttpResponse<Oferta[]>) => res);
   }
 
-  public getOfertasPorCategoria(categoria: string): Promise<Array<Oferta>> {
+  public getOfertasPorCategoria(
+    categoria: string
+  ): Promise<HttpResponse<Oferta[]>> {
     return this.http
       .get(`${URL_API}/ofertas?categoria=${categoria}`)
       .toPromise()
-      .then((res: any) => res);
+      .then((res: HttpResponse<Oferta[]>) => res);
   }
 
-  public getOfertaPorId(id: number): Promise<Oferta> {
+  public getOfertaPorId(id: number): Promise<HttpResponse<Oferta>> {
     return this.http
       .get(`${URL_API}/ofertas?id=${id}`)
       .toPromise()
-      .then((res: any) => res.shift());
+      .then((res: HttpResponse<Oferta>) => res);
   }
 
   public getComoUsarOfertaPorId(id: number): Promise<string> {
     return this.http
       .get(`${URL_API}/como-usar?id=${id}`)
       .toPromise()
-      .then((res: any) => {
+      .then((res: HttpResponse<String>) => {
         return res[0].descricao;
       });
   }
@@ -43,17 +45,20 @@ export class OfertasService {
     return this.http
       .get(`${URL_API}/onde-fica?id=${id}`)
       .toPromise()
-      .then((res: any) => {
+      .then((res: HttpResponse<String>) => {
         return res[0].descricao;
       });
   }
 
-  public pesquisaOfertas(termo: string): Observable<Oferta[]> {
+  public pesquisaOfertas(termo: string): Observable<HttpResponse<Oferta[]>> {
     return this.http
       .get(`${URL_API}/ofertas?descricao_oferta_like=${termo}`)
       .pipe(
         retry(10),
-        map((res: any) => res)
+        map((res: HttpResponse<Oferta[]>) => {
+          console.log(res);
+          return res;
+        })
       );
   }
 }
