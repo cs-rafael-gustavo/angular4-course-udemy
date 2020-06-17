3d46653bf37bd7c586ea9b537ff4b05b496ac4ce
Rafael Matoso <rafaelmatosog@gmail.com>
2020-06-17 20:27:31 -0300

    Lesson 178, 179 Done

--- 07_app2/passarin-urbano/src/app/pages/ordem-compra/ordem-compra.component.ts
+++ 07_app2/passarin-urbano/src/app/pages/ordem-compra/ordem-compra.component.ts
@@ -11,23 +11,63 @@ export class OrdemCompraComponent implements OnInit {
   public complemento: string = "";
   public formaPagamento: string = "";
 
+  public enderecoValido: boolean;
+  public numeroValido: boolean;
+  public complementoValido: boolean;
+  public formaPagamentoValido: boolean;
+
+  public enderecoEstadoPrimitivo: boolean = true;
+  public numeroEstadoPrimitivo: boolean = true;
+  public complementoEstadoPrimitivo: boolean = true;
+  public formaPagamentoEstadoPrimitivo: boolean = true;
+
   constructor() {}
 
   ngOnInit(): void {}
 
   public atualizaEndereco(endereco: string): void {
     this.endereco = endereco;
+
+    this.enderecoEstadoPrimitivo = false;
+
+    if (this.endereco.length > 3) {
+      this.enderecoValido = true;
+    } else {
+      this.enderecoValido = false;
+    }
   }
 
   public atualizaNumero(numero: string): void {
     this.numero = numero;
+
+    this.numeroEstadoPrimitivo = false;
+
+    if (this.numero !== "") {
+      this.numeroValido = true;
+    } else {
+      this.numeroValido = false;
+    }
   }
 
   public atualizaComplemento(complemento: string): void {
     this.complemento = complemento;
+
+    this.complementoEstadoPrimitivo = false;
+
+    if (this.complemento !== "") {
+      this.complementoValido = true;
+    }
   }
 
   public atualizaFormaPagamento(formaPagamento: string): void {
     this.formaPagamento = formaPagamento;
+
+    this.formaPagamentoEstadoPrimitivo = false;
+
+    if (this.formaPagamento !== "") {
+      this.formaPagamentoValido = true;
+    } else {
+      this.formaPagamentoValido = false;
+    }
   }
 }
