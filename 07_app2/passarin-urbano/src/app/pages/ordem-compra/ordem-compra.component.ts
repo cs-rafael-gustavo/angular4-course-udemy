34d13ff4a01a2c72b70ba4b052afeb2265aea874
Rafael Matoso <rafaelmatosog@gmail.com>
2020-06-19 12:04:14 -0300

    Lesson 209 to 211 Done

--- 07_app2/passarin-urbano/src/app/pages/ordem-compra/ordem-compra.component.ts
+++ 07_app2/passarin-urbano/src/app/pages/ordem-compra/ordem-compra.component.ts
@@ -25,11 +25,28 @@ export class OrdemCompraComponent implements OnInit {
     formaPagamento: new FormControl(null, Validators.required),
   });
 
+  public idPedidoCompra: number;
+
   constructor(private ordemCompraService: OrdemCompraService) {}
 
   ngOnInit() {}
 
   public confirmarCompra(): void {
-    console.log(this.formulario);
+    if (this.formulario.status === "INVALID") {
+      this.formulario.markAllAsTouched();
+    }
+
+    let pedido: Pedido = new Pedido(
+      this.formulario.value.endereco,
+      this.formulario.value.numero,
+      this.formulario.value.complemento,
+      this.formulario.value.formaPagamento
+    );
+
+    this.ordemCompraService
+      .efetivarCompra(pedido)
+      .subscribe((idPedido: number) => {
+        this.idPedidoCompra = idPedido;
+      });
   }
 }
