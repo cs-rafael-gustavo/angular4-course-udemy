a3e2e6ffd3a90f13eb2b333dd7f9a65793118222
Rafael Matoso <rafaelmatosog@gmail.com>
2020-06-21 11:48:09 -0300

    Lesson 220 Done

--- 07_app2/passarin-urbano/src/app/pages/ordem-compra/carrinho-compra/carrinho-compra.service.ts
+++ 07_app2/passarin-urbano/src/app/pages/ordem-compra/carrinho-compra/carrinho-compra.service.ts
@@ -17,6 +17,15 @@ export class CarrinhoCompraService {
       oferta.valor,
       1
     );
-    this.itens.push(itemCarrinho);
+
+    let itemCarrinhoEncontrado = this.itens.find(
+      (item: ItemCarrinho) => item.id === itemCarrinho.id
+    );
+
+    if (itemCarrinhoEncontrado) {
+      itemCarrinhoEncontrado.quantidade += 1;
+    } else {
+      this.itens.push(itemCarrinho);
+    }
   }
 }
