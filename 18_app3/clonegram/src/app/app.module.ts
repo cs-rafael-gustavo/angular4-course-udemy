d94e0bed63c5f3af924c44f9822df92ec90a9160
Rafael Matoso <rafaelmatosog@gmail.com>
2020-06-21 21:33:32 -0300

    Lesson 229 to 239 Done

---
+++ 18_app3/clonegram/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { AcessoComponent } from "./pages/acesso/acesso.component";
+import { BannerComponent } from "./pages/acesso/banner/banner.component";
+import { LoginComponent } from "./pages/acesso/login/login.component";
+import { CadastroComponent } from "./pages/acesso/cadastro/cadastro.component";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AcessoComponent,
+    BannerComponent,
+    LoginComponent,
+    CadastroComponent,
+  ],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
